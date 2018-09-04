6ac06d17a2d35040f61100890de9a818d4305bf5
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2018-09-04 12:34:25 +0200

    i18n

--- lang/de/qtype_diagram.php
+++ lang/de/qtype_diagram.php
@@ -1,17 +1,20 @@
 <?php
 
-$string['pluginname'] = 'Diagramm';
-$string['pluginnamesummary'] = 'Erlaubt das Erstellen und Abgeben eines Diagramms mit Draw.io innerhalb von Moodle.
-<br>Muss manuell bewertet werden.';
-$string['pluginname_help'] = 'Eine Frage wird mit einem Diagram beantwortet, welches mit Draw.io gezeichnet wird.
+$string['pluginname'] = 'Diagram';
+$string['pluginnamesummary'] = 'Erlaubt das Erstellen und Abgeben eines Diagrammes mit Draw.io innerhalb von Moodle.
+<br/>Muss manuell bewertet werden.
+<br/>
+<br/>
+Für die Abgabe kann ein Template angegeben werden';
+$string['pluginname_help'] = 'Eine Frage wird mit einem Diagramm beantwortet, welches mit Draw.io gezeichnet wird.
 <br>Das Diagram muss manuell bewertet werden.';
 $string['pluginname_link'] = 'question/type/diagram';
-$string['pluginnameadding'] = 'Diagram-Frage hinzufügen';
-$string['pluginnameediting'] = 'Diagram-Frage bearbeiten';
+$string['pluginnameadding'] = 'Diagramm-Frage hinzufügen';
+$string['pluginnameediting'] = 'Diagramm-Frage bearbeiten';
 $string['graderinfo'] = 'Bewertungshinweise';
-$string['default_diagram'] = 'Einstellungen';
+$string['default_diagram'] = 'Diagramm-Einstellungen';
 $string['selectedpalettes'] = 'Paletten';
 $string['selectedpalettes_help'] = 'Erlaubt das Angeben der angezeigten Paletten.
-<br>Mögliche Werte: general, images, uml, er, bpmn, flowchart, basic, arrows2, ios, android, mscae, eip, mockups, clipart, pid2, signs, rack, electrical, aws2, cisco, cabinets, floorplan, bootstrap, gmdl, archimate und sysml.
-<br>Können durch ; verknüpft werden';
-$string['privacy:metadata'] = 'Der Diagramm-Fragetyp-Plugin speichert keine persönlichen Daten.';
+<br/>Mögliche Werte: general, images, uml, er, bpmn, flowchart, basic, arrows2, ios, android, mscae, eip, mockups, clipart, pid2, signs, rack, electrical, aws2, cisco, cabinets, floorplan, bootstrap, gmdl, archimate und sysml.
+<br/>Mehrere Angeben müssen durch ; getrennt werden.';
+$string['privacy:metadata'] = 'Der Diagram-Fragetyp-Plugin speichert keine persönlichen Daten.';
--- lang/en/qtype_diagram.php
+++ lang/en/qtype_diagram.php
@@ -1,14 +1,20 @@
 <?php
 
 $string['pluginname'] = 'Diagram';
-$string['pluginnamesummary'] = 'Allows the student to submit a Diagram as response. Must be graded manually.';
-$string['pluginname_help'] = 'In response to a question the respondent draws a Diagram using Draw.io. The Diagram will not be assigned a grade until it has been reviewed and manually graded by a teacher.';
+$string['pluginnamesummary'] = 'Allows the creation and submission of a Diagram created with embedded draw.io.
+<br/>Must be graded manually.
+<br/>
+<br/>
+A template can be specified for editing';
+$string['pluginname_help'] = 'In response to a question the respondent draws a Diagram using Draw.io.
+<br/>The Diagram will not be assigned a grade until it has been reviewed and manually graded by a teacher.';
 $string['pluginname_link'] = 'question/type/diagram';
 $string['pluginnameadding'] = 'Adding a Diagram question';
 $string['pluginnameediting'] = 'Editing a Diagram question';
 $string['graderinfo'] = 'Information for grader';
-$string['default_diagram'] = 'Default diagram';
-$string['selectedpalettes'] = 'Paletten';
-$string['selectedpalettes_help'] = 'Possible values: general, images, uml, er, bpmn, flowchart, basic, arrows2, ios, android, mscae, eip, mockups, clipart, pid2, signs, rack, electrical, aws2, cisco, cabinets, floorplan, bootstrap, gmdl, archimate and sysml.
-Können durch ; verknüpft werden';
+$string['default_diagram'] = 'Diagram-Settings';
+$string['selectedpalettes'] = 'Palettes';
+$string['selectedpalettes_help'] = 'Allows to specify the used palettes
+<br/>Possible values: general, images, uml, er, bpmn, flowchart, basic, arrows2, ios, android, mscae, eip, mockups, clipart, pid2, signs, rack, electrical, aws2, cisco, cabinets, floorplan, bootstrap, gmdl, archimate and sysml.
+<br/>Several entries must be separated by ;.';
 $string['privacy:metadata'] = 'The Diagram question type plugin does not store any personal data.';
